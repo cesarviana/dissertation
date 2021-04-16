11e27dd66d7bc4921cfc8afd3cf5e34b01be4d0c
Cesar Pereira Viana <cesar.pviana@gmail.com>
2021-04-16 14:15:43 -0300

    Added [re]build scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
 pdflatex -syntex=1 -shell-escape -interaction=nonstopmode 00_main.tex
-bibtex 00_main.aux
-pdflatex -syntex=1 -shell-escape -interaction=nonstopmode 00_main.tex
-pdflatex -syntex=1 -shell-escape -interaction=nonstopmode 00_main.tex
